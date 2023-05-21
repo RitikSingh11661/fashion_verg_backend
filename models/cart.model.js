805a9771ffa14f9b35dc2c39d17616912df0e91d
ritiksingh11661 <delhi8260@gmail.com>
2023-05-21 15:56:42 +0530

    change in cart model

--- models/cart.model.js
+++ models/cart.model.js
@@ -5,34 +5,16 @@ const schema = mongoose.Schema({
     prodId: String,
     name: String,
     image: Object,
-    brand: String,
-    originalPrice: Number,
-    discountPrice: Number,
+    oprice: Number,
+    price: Number,
     category: String,
-    quantity: Number
+    quantity: Number,
+    description: String,
+    discount: String,
+    subhead: String,
+    status: String,
 })
 
 const cartModel = mongoose.model("cart", schema);
 
 module.exports = { cartModel };
-
-
-// "userId": "64268574502c3084e556d596",
-//     "name": "Sofa",
-//     "image": "http://link.com",
-//     "brand": "Sofa brand",
-//     "originalPrice": 123,
-//     "discountPrice": 32,
-//     "category": furniture
-
-// {
-//     "prodId":"64268bdf5c451fa3d60665fb",
-//     "userId": "64268574502c3084e556d596",
-//       "name": "Sofa",
-//       "image": "http://link.com",
-//       "brand": "Sofa brand",
-//       "originalPrice": 123,
-//       "discountPrice": 32,
-//       "category": "furniture"
-//       "quantity":4
-//   }
