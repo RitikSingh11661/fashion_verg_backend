72c862b3b9ce631e902a2ea9aacf633c5436a413
ritiksingh11661 <delhi8260@gmail.com>
2023-05-22 18:38:38 +0530

    expiration time extended

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -41,7 +41,7 @@ userRoutes.post("/login", async(req, res)=>{
             const preCheck = await userModel.findOne({ email });
             if (preCheck) {
                 const hashCheck = await bcrypt.compare(password, preCheck.password);
-                const token = jwt.sign({ "userId": preCheck._id,role:preCheck.role},'Fashion', { expiresIn: "2h" });
+                const token = jwt.sign({ "userId": preCheck._id,role:preCheck.role},'Fashion', { expiresIn: "5h" });
                 if (hashCheck)res.status(200).send({msg:"User logged in",status:"success",token,role:preCheck.role});
                 else res.status(400).send({ msg: "Invalid password" });
             } else res.status(400).send({ msg: "User not found" });
